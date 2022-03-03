cdb56adb090d0b0c7bbf6a9dbdac147ed3920698
mstich02 <42916761+mstich02@users.noreply.github.com>
2022-03-03 13:44:47 -0700

    Add files via upload

---
+++ winCheck.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+// x is 19 o is 11
+//We used a prime number to calculate a win condition
+int diagonalWin(int matrix[3][3],int size,int dimention){
+	int count;
+	int count2;
+	int horizontalLR = 0;
+	int horizontalRL = 0;
+	int win1;
+	int win2;
+	for(count = dimention;count > 0;count--){
+		for (count = (size-1); count>=0;count--){
+			horizontalLR = horizontalLR + matrix[count][count];  
+		} 
+		for (count = (size-1); count>=0;count--){
+			horizontalRL = horizontalRL + matrix[count][(size-(count+1))];
+		}	
+		win1 = winCheck(horizontalLR,size,dimention);
+		win2 = winCheck(horizontalRL,size,dimention);
+	
+		if (win1 > 0){
+			return win1;
+		}
+		else if (win2 > 0){
+			return win2;
+		}
+		else{
+			return (win1+win2);
+		}
+	}
+}
+int horizontalWin(int matrix[3][3],int size,int dimention){
+	int row;
+	int col;
+	int count;
+	int win = 0; 
+	int total = 0;
+	row = (size-1);
+	for(count = dimention; count > 0; count--){
+		do{
+			for (col = (size-1); col>=0;col--){
+				total = total + matrix[row][col];
+			}
+			win = winCheck(total,size,dimention);
+			row--;
+		}while ((row >  0)||(win==0));
+		return win;
+	}
+}
+int verticalWin(int matrix[3][3],int size,int dimention){
+        int row;
+        int col;
+        int win = 0;
+        int total = 0;
+        col = (size-1);
+        do{
+                for (row = (size-1); row>=0;row--){
+                        total = total + matrix[row][col];
+                }
+                win = winCheck(total,size,dimention);
+                col--;
+       	}while ((col >  0)||(win==0));
+        return win;
+}
+
+
+int winCheck(int total, int size,int dimention){
+	float win = 0;
+	win = total/size;
+	if (win == 19){
+		return 1;
+	}
+	else if (win == 11){
+		return 2;
+	}	 
+	else{
+		return 0;
+	}
+}
+
+int main(){
+	int mat1[3][3] = {{0,0,0},{0,0,0},{0,0,0}};
+	int mat2[3][3] = {{19,0,0},{0,19,0},{0,0,19}};
+	int mat3[3][3] = {{19,19,19},{0,0,0},{0,0,0}};
+	int mat4[3][3] = {{11,0,0},{11,0,0},{11,0,0}};
+	int currentMat = mat1;
+	printf("%d\n",diagonalWin(currentMat,3,1));
+	printf("%d\n",horizontalWin(currentMat,3,1));
+	printf("%d\n",verticalWin(currentMat,3,1));
+	return 0;
+}
