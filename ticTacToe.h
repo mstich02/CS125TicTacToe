dc89afc1524f10c549bbdaf29219e8a45156d0da
mstich02 <42916761+mstich02@users.noreply.github.com>
2022-03-08 13:33:40 -0700

    Add files via upload

---
+++ ticTacToe.h
@@ -0,0 +1,8 @@
+#ifdef TICTACTOE_DOT_H
+#define TICTACTOE_DOT_H
+
+int diagonalWin(int matrix[3][3], int size, int dimention);
+int horizontalWin(int matrix[3][3], int size, int dimention);
+int verticalWin(int matrix[3][3], int size, int dimention);
+
+#endif 
