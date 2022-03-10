9cb839430329e372f5e6199da562588c08cccab0
mstich02 <42916761+mstich02@users.noreply.github.com>
2022-03-10 13:26:15 -0700

    Add files via upload

---
+++ AI.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <time.h>
+
+int easyMode(int matrix, int size, int dimention){
+	int randX = rand()%(size);
+	int randY = rand()%(size);
+	int randZ = rand()%(dimention);
+	
+	while (errorCheck(randY,randX,randZ) == 0){
+      		int randX = rand()%(size);
+       		int randY = rand()%(size);
+	        int randZ = rand()%(dimention);
+	}
+//	return position
+}
+
+int hardMode(matrix, size, dimentions, turn
+// if player places on corner
+int matrix
+int size
+	if(matrix[0][0] = change 
+	 
+// i fplayer places on edge
+// if player places on middle
+ 
+int main(){
+	srand(time(NULL));
+}
