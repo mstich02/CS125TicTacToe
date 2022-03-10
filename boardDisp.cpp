c598938f1c31266897cf90170c56ed1e825cd4a7
jashock <jameson0212@gmail.com>
2022-03-10 14:15:35 -0700

    Add files via upload

--- boardDisp.cpp
+++ boardDisp.cpp
@@ -3,14 +3,16 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include "ticTacToe.h"
 
 int rowput;
 int colput;
-
-char row1[3] = { ' ',' ',' ' };
-char row2[3] = { ' ',' ',' ' };
-char row3[3] = { ' ',' ',' ' };
-int col;
+											
+char row1[3] = { ' ',' ',' ' };           //-------------------------------//
+char row2[3] = { ' ',' ',' ' };			 // Setup the board in a 3x3 grid //
+char row3[3] = { ' ',' ',' ' };			//                               //
+int col;							   //-------------------------------//
+int matrix[3][3] = { {' ',' ',' '},{' ',' ',' '},{' ',' ',' '} };
 int validCheck = 0;
 
 
@@ -19,29 +21,30 @@ int turntime;
 
 int main()
 {
-printf(" 1     2    3\n");
+printf("COL> 1   2  3\n"); // Label the columns
+printf("ROW v"); //Label the rows
 printf("\n");
-printf("1 %c  | %c | %c\n", row1[0], row1[1], row1[2]);
-printf("  -----------\n");
-printf("2 %c  | %c | %c\n", row2[0], row2[1], row2[2]);
-printf("  -----------\n");
-printf("3 %c  | %c | %c\n", row3[0], row3[1], row3[2]);
+printf("  1 %c  | %c | %c\n", row1[0], row1[1], row1[2]);			 //--------------------------------//
+printf("    -----------\n");										//                                //
+printf("  2 %c  | %c | %c\n", row2[0], row2[1], row2[2]);		   // Represent the board with ASCII //
+printf("    -----------\n");									  //                                //
+printf("  3 %c  | %c | %c\n", row3[0], row3[1], row3[2]);		 //--------------------------------//
 for (turntime = 0; turntime < 9; turntime++)
 {
 
-	if (turntime % 2 == 0)
+	if (turntime % 2 == 0) //check who's turn it is X or Y
 	{
-		validCheck = 0;
-		printf("It is X's turn\n");
-		while (validCheck == 0)
+		validCheck = 0; //reset valid check
+		printf("It is X's turn\n"); 
+		while (validCheck == 0) //Check that the square is a valid choice to avoid overwriting
 		{
-			printf("which row would you like it in?: ");
+			printf("which row would you like it in?: "); //Row input
 			scanf("%d", &rowput);
 
-			printf("which column would you like it in?: ");
+			printf("which column would you like it in?: "); //Col input
 			scanf("%d", &colput);
 
-			if (rowput == 1)
+			if (rowput == 1) //Row 1 checker
 			{
 				if (row1[colput - 1] == 'x')
 				{
@@ -59,7 +62,7 @@ for (turntime = 0; turntime < 9; turntime++)
 				}
 			}
 
-			else if (rowput == 2)
+			else if (rowput == 2) //Row 2 checker
 			{
 				if (row2[colput - 1] == 'x')
 				{
@@ -76,7 +79,7 @@ for (turntime = 0; turntime < 9; turntime++)
 					validCheck = 1;
 				}
 			}
-			else if (rowput == 3)
+			else if (rowput == 3) //Row 3 checker
 			{
 				if (row3[colput - 1] == 'x')
 				{
@@ -99,21 +102,24 @@ for (turntime = 0; turntime < 9; turntime++)
 			}
 
 		}
-		if (rowput == 1)
+		if (rowput == 1) //Input an x in the valid spots
 		{
 			row1[colput - 1] = 'x';
+			matrix[0][colput - 1] = 19;
 		}
 		else if (rowput == 2)
 		{
 			row2[colput - 1] = 'x';
+			matrix[1][colput - 1] = 19;
 		}
 		else if (rowput == 3)
 		{
 			row3[colput - 1] = 'x';
+			matrix[2][colput - 1] = 19;
 		}
-		else printf("INVALID INPUT");
+		else printf("INVALID INPUT"); 
 	}
-	else if (turntime % 2 != 0)
+	else if (turntime % 2 != 0) //Same thing but for Y
 	{
 		validCheck = 0;
 		printf("It is Y's turn\n");
@@ -187,23 +193,31 @@ for (turntime = 0; turntime < 9; turntime++)
 			if (rowput == 1)
 			{
 				row1[colput - 1] = 'y';
+				matrix[0][colput - 1] = 11;
 			}
 			else if (rowput == 2)
 			{
 				row2[colput - 1] = 'y';
+				matrix[1][colput - 1] = 11;
 			}
 			else if (rowput == 3)
 			{
 				row3[colput - 1] = 'y';
+				matrix[2][colput - 1] = 11;
 			}
 			else printf("INVALID INPUT");
 	}
-	printf(" 1     2    3\n");
+	printf("COL> 1   2  3\n"); //Print the current state of the board, go back to the top and swap turns
+	printf("ROW v");
 	printf("\n");
-	printf("1 %c  | %c | %c\n", row1[0], row1[1], row1[2]);
-	printf("  -----------\n");
-	printf("2 %c  | %c | %c\n", row2[0], row2[1], row2[2]);
-	printf("  -----------\n");
-	printf("3 %c  | %c | %c\n", row3[0], row3[1], row3[2]);
+	printf("  1 %c  | %c | %c\n", row1[0], row1[1], row1[2]);
+	printf("    -----------\n");
+	printf("  2 %c  | %c | %c\n", row2[0], row2[1], row2[2]);
+	printf("    -----------\n");
+	printf("  3 %c  | %c | %c\n", row3[0], row3[1], row3[2]);
+
+	horizontalWin(matrix, 3, 1);
+	verticalWin(matrix, 3, 1);
+	diagonalWin(matrix, 3, 1);
 	}
 }
