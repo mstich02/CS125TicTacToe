a4643d3300cb4f33859e1569031b128a53e806df
jashock <jameson0212@gmail.com>
2022-03-11 15:47:37 -0700

    Add files via upload

--- ticTacToe.h
+++ ticTacToe.h
@@ -1,8 +1,5 @@
-#ifdef TICTACTOE_DOT_H
-#define TICTACTOE_DOT_H
+#pragma once
 
 int diagonalWin(int matrix[3][3], int size, int dimention);
 int horizontalWin(int matrix[3][3], int size, int dimention);
 int verticalWin(int matrix[3][3], int size, int dimention);
-
-#endif 
