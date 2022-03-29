07b812894d02cb20b5c48381defdce6709584d02
jashock <jameson0212@gmail.com>
2022-03-29 14:39:11 -0700

    Add files via upload

--- AI.c
+++ AI.c
@@ -1,31 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 #include <time.h>
+#include <ctype.h>
+#include "ticTacToe.h"
 
-int easyMode(int matrix, int size, int dimention){
-	int randX = rand()%(size);
-	int randY = rand()%(size);
-	int randZ = rand()%(dimention);
-	
-	while (errorCheck(randY,randX,randZ) == 0){
-      		int randX = rand()%(size);
-       		int randY = rand()%(size);
-	        int randZ = rand()%(dimention);
-	}
-//	return position
+int gameModeStart() {
+    char gameMode;
+    int exit = 0;
+
+    printf("Do you want to play single or multiplayer? (s/m)\n");
+    scanf("%c", &gameMode);
+    gameMode = tolower(gameMode);
+
+    while (exit == 0) {
+        if ((gameMode == 's') || (gameMode == 'm')) {
+            exit = 1;
+            if (gameMode == 'm') {
+                printf("You have chosen multiplayer\n");
+                return 0;
+            }
+            else {
+                printf("You have chosen singleplayer\n");
+                return 1;
+            }
+        }
+    }
 }
 
-int hardMode(matrix, size, dimentions, turn
-// if player places on corner
-int matrix
-int size
-	if(matrix[0][0] = change 
-	 
-// i fplayer places on edge
-// if player places on middle
- 
-int main(){
-	srand(time(NULL));
+int errorCheck(int matrix[3][3], int x, int y, int z) {
+    int	validCheck = 0;
+
+    if (((matrix[(y - 1)][(x - 1)]) == 11) || ((matrix[(y - 1)][(x - 1)]) == 19)) {
+        return 0;
+    }
+    else {
+        return 1;
+    }
+}
+mat easyMode(int matrix[3][3], int size, int dimention) {
+    mat position;
+    srand(time(NULL));
+    int randX = rand() % (size);
+    int randY = rand() % (size);
+    int randZ = rand() % (dimention);
+
+    while (errorCheck(matrix, randY, randX, randZ) == 0) {
+        int randX = rand() % (size);
+        int randY = rand() % (size);
+        int randZ = rand() % (dimention);
+    }
+    //	return position
+    position.row = randY + 1;
+    position.col = randX + 1;
+    return position;
+
 }
--- ticTacToe.h
+++ ticTacToe.h
@@ -1,5 +1,14 @@
 #pragma once
 
+typedef struct returnPosition {
+	int row;
+	int col;
+}mat;
+
 int diagonalWin(int matrix[3][3], int size, int dimention);
 int horizontalWin(int matrix[3][3], int size, int dimention);
 int verticalWin(int matrix[3][3], int size, int dimention);
+
+int gameModeStart();
+mat easyMode(int matrix[3][3], int size, int dimention);
+
